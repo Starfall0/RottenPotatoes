7cd007f8d5580d40a98ba31e3dbc57f038b01764
Starfall0 <theradeth@gmail.com>
2024-02-23 04:40:50 +0000

    new

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 # Replace API_KEY and API_SECRET with the values you got from Google
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :google_oauth2, '885724697055-f8bi2ip1gte912u9pngrhrk4urk66o86.apps.googleusercontent.com', 
-    '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', 
+    provider :google_oauth2, 'YOUR_API_KEY', 
+    'YOUR_API_SECRET', 
     scope: 'email, profile'
   end
   OmniAuth.config.allowed_request_methods = %i[get]
